f31997f571c504c0c61e13c3435f64e481677346
ravivanco <90229638+ravivanco@users.noreply.github.com>
2025-06-12 19:43:18 -0500

    v2: área del traingulo

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,15 @@
 def area_cuadrado(lado):
     return lado * lado
 
+def area_triangulo(base, altura):
+    return (base * altura) / 2
+
+# Área cuadrado
 lado = float(input("Ingrese el lado del cuadrado: "))
 print("Área del cuadrado:", area_cuadrado(lado))
+
+# Área triángulo
+base = float(input("Ingrese la base del triángulo: "))
+altura = float(input("Ingrese la altura del triángulo: "))
+print("Área del triángulo:", area_triangulo(base, altura))
+
