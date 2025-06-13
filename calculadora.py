25cfa9b40e942d56370ac64c2ace540ecd33ac25
ravivanco <90229638+ravivanco@users.noreply.github.com>
2025-06-12 19:44:54 -0500

    v3: área del rectangulo

--- calculadora.py
+++ calculadora.py
@@ -4,6 +4,9 @@ def area_cuadrado(lado):
 def area_triangulo(base, altura):
     return (base * altura) / 2
 
+def area_rectangulo(base, altura):
+    return base * altura
+
 # Área cuadrado
 lado = float(input("Ingrese el lado del cuadrado: "))
 print("Área del cuadrado:", area_cuadrado(lado))
@@ -13,3 +16,7 @@ base = float(input("Ingrese la base del triángulo: "))
 altura = float(input("Ingrese la altura del triángulo: "))
 print("Área del triángulo:", area_triangulo(base, altura))
 
+# Área rectángulo
+base_r = float(input("Ingrese la base del rectángulo: "))
+altura_r = float(input("Ingrese la altura del rectángulo: "))
+print("Área del rectángulo:", area_rectangulo(base_r, altura_r))
